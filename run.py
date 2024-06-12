fd1d0aacbbb56acd306b522d6a2f80d8e937df64
nguyenha-meiii <nguyenthuhat1k60@gmail.com>
2024-06-12 17:23:12 +0700

    update for security

--- run.py
+++ run.py
@@ -1,25 +1,42 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import logging
 import json
 import os
 
+
 # Load environment variables
 MONGO_URI = os.getenv('DATABASE_PORT', 'mongodb+srv://root:hello123@vdt.2w2zlck.mongodb.net/?retryWrites=true&w=majority&appName=vdt')
 JWT_SECRET = os.getenv('JWT_SECRET', 'VDT2024')
 
+
 # Initialize Flask app
 app = Flask(__name__)
 cors = CORS(app, resource={
-    r"/*":{
-        "origins":"*"
+    r"/*": {
+        "origins": "*"
     }
 })
 
+
 # Set up logging
 logging.basicConfig(level=logging.INFO)
+
+
+# Initialize Limiter
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["10 per minute"],
+    storage_uri="memory://"
+)
+
+
+limiter.init_app(app)
+
 
 # MongoDB configuration function
 def get_db():
@@ -27,21 +44,25 @@ def get_db():
     db = client["test"]
     return db
 
-# Routes 
+
+# Routes
+
 
 @app.route('/')
+@limiter.limit("10 per minute")
 def ping_server():
     db = get_db()
     trainees_collection = db.trainees
-    # lỗi không connect được vs database
     trainees = list(trainees_collection.find({}, {'_id': 1, 'name': 1, 'email': 1, 'gender': 1, 'school': 1}))
     for trainee in trainees:
         trainee['_id'] = str(trainee['_id'])
-    logging.info("Hello123")  # sử dụng logging thay vì print
-    logging.info(trainees_collection)  # sử dụng logging thay vì print
+    logging.info("Hello123")
+    logging.info(trainees_collection)
     return "Welcome to trainees list."
 
+
 @app.route('/api/trainees', methods=['GET'])
+@limiter.limit("10 per minute")
 def get_trainees():
     try:
         db = get_db()
@@ -50,6 +71,7 @@ def get_trainees():
         for trainee in trainees:
             trainee['_id'] = str(trainee['_id'])  # Convert ObjectId to string
 
+
         formatted_response = '[' + ',\n'.join(json.dumps(trainee, ensure_ascii=False) for trainee in trainees) + ']'
         response = Response(formatted_response, content_type='application/json; charset=utf-8')
         return response
@@ -57,7 +79,9 @@ def get_trainees():
         logging.error(f"Error fetching trainees: {e}")
         return jsonify({'message': 'Internal server error'}), 500
 
+
 @app.route('/api/trainees', methods=['POST'])
+@limiter.limit("10 per minute")
 def create_trainee():
     try:
         db = get_db()
@@ -69,7 +93,9 @@ def create_trainee():
         logging.error(f"Error creating trainee: {e}")
         return jsonify({'message': 'Internal server error'}), 500
 
+
 @app.route('/api/trainees/<string:id>', methods=['GET'])
+@limiter.limit("10 per minute")
 def get_trainee(id):
     try:
         db = get_db()
@@ -84,7 +110,9 @@ def get_trainee(id):
         logging.error(f"Error fetching trainee with ID {id}: {e}")
         return jsonify({'message': 'Internal server error'}), 500
 
+
 @app.route('/api/trainees/<string:id>', methods=['PUT'])
+@limiter.limit("10 per minute")
 def update_trainee(id):
     try:
         db = get_db()
@@ -100,7 +128,9 @@ def update_trainee(id):
         logging.error(f"Error updating trainee with ID {id}: {e}")
         return jsonify({'message': 'Internal server error'}), 500
 
+
 @app.route('/api/trainees/<string:id>', methods=['DELETE'])
+@limiter.limit("10 per minute")
 def delete_trainee(id):
     try:
         db = get_db()
@@ -113,6 +143,7 @@ def delete_trainee(id):
     except Exception as e:
         logging.error(f"Error deleting trainee with ID {id}: {e}")
         return jsonify({'message': 'Internal server error'}), 500
+
 
 # Run the Flask app
 if __name__ == '__main__':
